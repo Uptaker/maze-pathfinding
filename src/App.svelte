764a825b1a41294aad65483834a60c75ff90a23f
Uptaker <markus.tammeoja@gmail.com>
2022-10-24 20:17:19 +0300

    reuse bloxorz prototype for maze

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Maze from "./lib/Maze.svelte";
+</script>
+
+<main>
+  <div class="card">
+    <Maze />
+  </div>
+</main>
---
+++ src/lib/Maze.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+
+  import {onMount} from 'svelte'
+
+  enum Type {
+    START = 'S', FINISH = 'F', WHITE = 'W', GRAY = 'G', BLACK = 'B', OBSTACLE = 'O'
+  }
+
+  interface Position {
+    row: number,
+    column: number
+  }
+
+  interface Tile {
+    type: Type,
+  }
+
+  const stateAsStrings: string[] = [
+    '.S...OOOOO',
+    '...OOOO.F.',
+    '....OO....',
+    '.........O',
+    'O......OOO',
+    'OOO....OOO'
+  ]
+
+  const state: Tile[][] = stringToTile(stateAsStrings)
+
+  onMount(() => console.log(state))
+
+  function stringToTile(strings: string[]): Tile[][] {
+    let startingState: Tile[][] = []
+    strings.forEach((r, x) => {
+      const row = []
+      const column = r.split("")
+      column.forEach((c, y) => row.push({type: c} as Tile))
+      startingState.push(row)
+    })
+    return startingState
+  }
+
+  function color(value: Tile) {
+    switch (value.type) {
+      case Type.GRAY: return 'gray'
+      case Type.WHITE: return 'white'
+      case Type.BLACK: return 'black'
+      case Type.START: return 'yellow'
+      case Type.FINISH: return 'green'
+      case Type.OBSTACLE: return 'beige'
+    }
+  }
+
+</script>
+
+<div class="board" style="margin-bottom: 5px">
+    {#if state}
+        <div class="row">
+            {#each state as row, i}
+                <div class="column">
+                    {#each row as value, j}
+                        <div class="square" style="background-color: {color(value)}">
+                            {#if value.type === Type.START || value.type === Type.FINISH}
+                                <b>{value.type === Type.START ? 'Start' : 'Finish'}</b>
+                            {/if}
+                        </div>
+                    {/each}
+                </div>
+            {/each}
+        </div>
+    {/if}
+</div>
+
+<button>Move</button>
+
+<style>
+    .row {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .column {
+        display: flex;
+    }
+
+    .square {
+        width: 50px;
+        height: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: 1px solid #bfbfbf
+    }
+
+    .board {
+        width: min-content;
+        margin: 0 auto;
+    }
+</style>
