eb2157022317ef6a351e881b0b4c12e88ea7b4ce
Uptaker <markus.tammeoja@gmail.com>
2022-10-25 21:26:51 +0300

    add ability to log current maze to console

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import type {Tile} from './types'
+
 export function sleep(ms, signal) {
   return new Promise<void>((resolve, reject) => {
     const timeout = setTimeout(() => {
@@ -11,4 +13,10 @@ export function sleep(ms, signal) {
     if (signal.aborted) abort();
     else signal.addEventListener('abort', abort)
   });
+}
+
+export function logTilesToStrings(tiles: Tile[][]) {
+  for (let i = 0; i < tiles.length; i++) {
+    console.log(tiles[i].map(t => t.type).join(''))
+  }
 }
